ee23ddf569aa6721fa06e7b152c33f71cabd9948
cner-smith <cner.smith+github@gmail.com>
2026-05-23 01:09:44 -0500

    feat: count tracker with balls, strikes, outs, fouls, and signals

---
+++ dngrz/src/core/count_tracker.gd
@@ -0,0 +1,49 @@
+class_name CountTracker
+
+signal walk
+signal strikeout
+signal out_recorded
+signal side_retired
+
+var balls: int = 0
+var strikes: int = 0
+var outs: int = 0
+
+func add_ball() -> void:
+	balls += 1
+	if balls >= 4:
+		walk.emit()
+
+func add_strike() -> void:
+	strikes += 1
+	if strikes >= 3:
+		add_out()
+		strikeout.emit()
+
+func add_foul() -> void:
+	if strikes < 2:
+		strikes += 1
+
+func add_out() -> void:
+	outs += 1
+	out_recorded.emit()
+	if outs >= 3:
+		side_retired.emit()
+
+func is_walk() -> bool:
+	return balls >= 4
+
+func is_strikeout() -> bool:
+	return strikes >= 3
+
+func is_side_retired() -> bool:
+	return outs >= 3
+
+func new_batter() -> void:
+	balls = 0
+	strikes = 0
+
+func new_half_inning() -> void:
+	balls = 0
+	strikes = 0
+	outs = 0
---
+++ dngrz/test/test_count_tracker.gd
@@ -0,0 +1,69 @@
+class_name TestCountTracker extends GdUnitTestSuite
+
+var _tracker: CountTracker
+
+func before_test() -> void:
+	_tracker = CountTracker.new()
+
+func test_starts_at_zero() -> void:
+	assert_int(_tracker.balls).is_equal(0)
+	assert_int(_tracker.strikes).is_equal(0)
+	assert_int(_tracker.outs).is_equal(0)
+
+func test_ball_increments() -> void:
+	_tracker.add_ball()
+	assert_int(_tracker.balls).is_equal(1)
+
+func test_four_balls_is_walk() -> void:
+	for i in 4:
+		_tracker.add_ball()
+	assert_bool(_tracker.is_walk()).is_true()
+
+func test_strike_increments() -> void:
+	_tracker.add_strike()
+	assert_int(_tracker.strikes).is_equal(1)
+
+func test_three_strikes_is_strikeout() -> void:
+	for i in 3:
+		_tracker.add_strike()
+	assert_bool(_tracker.is_strikeout()).is_true()
+
+func test_strikeout_adds_out() -> void:
+	for i in 3:
+		_tracker.add_strike()
+	assert_int(_tracker.outs).is_equal(1)
+
+func test_foul_with_two_strikes_stays_at_two() -> void:
+	_tracker.add_strike()
+	_tracker.add_strike()
+	_tracker.add_foul()
+	assert_int(_tracker.strikes).is_equal(2)
+
+func test_foul_with_less_than_two_strikes_adds_strike() -> void:
+	_tracker.add_foul()
+	assert_int(_tracker.strikes).is_equal(1)
+
+func test_new_batter_resets_count() -> void:
+	_tracker.add_ball()
+	_tracker.add_strike()
+	_tracker.new_batter()
+	assert_int(_tracker.balls).is_equal(0)
+	assert_int(_tracker.strikes).is_equal(0)
+
+func test_new_batter_preserves_outs() -> void:
+	_tracker.add_out()
+	_tracker.new_batter()
+	assert_int(_tracker.outs).is_equal(1)
+
+func test_three_outs_is_side_retired() -> void:
+	for i in 3:
+		_tracker.add_out()
+	assert_bool(_tracker.is_side_retired()).is_true()
+
+func test_new_half_inning_resets_outs() -> void:
+	_tracker.add_out()
+	_tracker.add_out()
+	_tracker.new_half_inning()
+	assert_int(_tracker.outs).is_equal(0)
+	assert_int(_tracker.balls).is_equal(0)
+	assert_int(_tracker.strikes).is_equal(0)
